0b1db7b2a85f0d5a2b8385118c921d7c25273d99
zeeshanhaider538 <zeeshanhaider538@gmail.com>
2022-12-09 23:38:18 +0500

    StairCase Completely worked and 100% accurate

--- Staircase.cs
+++ Staircase.cs
@@ -4,11 +4,37 @@ public class stairCase
 {
     public static void stc()
     {
+        // char a='#';
+        // int i;
+        // int k=0;
+        // int j=6;
+        // for(i=1;i<7;i++)
+        // {
+        //     while(j>i)
+        //     {
+        //         Console.Write(" ");
+        //         j--;
+        //     }
+        //     while(k<i)
+        //     {
+        //         Console.Write(a);
+        //         k++;
+
+        //     }
+            
+        //     Console.WriteLine("\n");
+        //     j=6;
+        //     k=0;
+        // }
+
+        //--------------  Hacker Rank Problem by Taking Input n gives 100% result----------------
+
         char a='#';
+        int n=Convert.ToInt32(Console.ReadLine());
         int i;
         int k=0;
-        int j=6;
-        for(i=1;i<7;i++)
+        int j=n;    // taking n as input
+        for(i=1;i<n+1;i++)
         {
             while(j>i)
             {
@@ -19,11 +45,9 @@ public class stairCase
             {
                 Console.Write(a);
                 k++;
-
             }
-            
-            Console.WriteLine("\n");
-            j=6;
+            Console.Write("\n");
+            j=n;
             k=0;
         }
     }
